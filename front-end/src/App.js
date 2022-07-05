72864680dd673bfbcfbf4de4ab71438836133a83
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2022-07-05 22:02:08 +0200

    Items on homescreen page with fake datas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,31 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Cash-out!</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Produits du moment</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.id}>
+              <a href={`/product/${product.id}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.id}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}</strong>€
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
