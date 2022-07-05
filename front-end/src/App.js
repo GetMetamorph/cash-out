450a533630333f66720e9ecd1d3e893c15606bb0
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2022-07-05 22:39:01 +0200

    added Routing and HomeScreens ProductScreen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,22 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Cash-out!</a>
-      </header>
-      <main>
-        <h1>Produits du moment</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.id}>
-              <a href={`/product/${product.id}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.id}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>{product.price}</strong>€
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Cash-out!</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
