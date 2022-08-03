d1c8132edf553ff3431c9a45e559b9b0486e30c9
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2022-08-03 13:52:59 +0200

    Added Reducer logger and patch for reducer logger modules

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Site e-commerce dans le cadre du Bachelor IPSSI 2022 par Driss Kanouni
 # How to use
 
 1. npm install
-2. npm start
+2. npm start coté front-end et back-end
--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,14 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import { Link } from 'react-router-dom';
+import logger from 'use-reducer-logger';
 import axios from 'axios';
+
 //import data from '../data';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAILED':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  //For Use Reducer
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
+    products: [],
+    error: '',
+  });
+
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAILED', payload: err.message });
+      }
+      //setProducts(result.data);
     };
     fetchData();
   }, []);
