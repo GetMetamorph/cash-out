abf5ed6ca8f692eea7795cd87eda1c3afe34698e
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2022-08-05 19:01:44 +0200

    Added: CartScreen to show list of items in the cart + button to checkout order and implement route to cart

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import { Badge, Nav } from 'react-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -51,6 +52,7 @@ function App() {
             <Routes>
               <Route path="/" element={<HomeScreen />} />
               <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart/" element={<CartScreen />} />
             </Routes>
           </Container>
         </main>
--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -9,7 +9,7 @@ import {
   ListGroupItem,
   Button,
 } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import logger from 'use-reducer-logger';
 import Rating from '../components/Rating';
@@ -33,6 +33,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
   const [{ loading, error, product }, dispatch] = useReducer(logger(reducer), {
@@ -70,6 +71,9 @@ function ProductScreen() {
       type: 'CART_ADD_ITEM',
       payload: { ...product, quantity },
     });
+
+    //Coming from react hook
+    navigate('/cart');
   };
 
   return loading ? (
