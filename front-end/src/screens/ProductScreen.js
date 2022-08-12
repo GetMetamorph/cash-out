7bf998886fc432e5a83075d3f6897fb7e31e2eea
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2022-08-12 10:07:18 +0200

    Implemented rate and reviews

--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef, useState, useContext } from 'react';
 import {
   Card,
   Col,
@@ -8,16 +8,18 @@ import {
   Badge,
   ListGroupItem,
   Button,
+  Form,
 } from 'react-bootstrap';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 //import logger from 'use-reducer-logger';
 import Rating from '../components/Rating';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
-import { useContext } from 'react';
 import { Store } from '../Store';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import { toast } from 'react-toastify';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -27,21 +29,33 @@ const reducer = (state, action) => {
       return { ...state, product: action.payload, loading: false };
     case 'FETCH_FAILED':
       return { ...state, loading: false, error: action.payload };
+    case 'REFRESH_PRODUCT':
+      return { ...state, product: action.payload };
+    case 'CREATE_REQUEST':
+      return { ...state, loadingCreateReview: true };
+    case 'CREATE_SUCCESS':
+      return { ...state, loadingCreateReview: false };
+    case 'CREATE_FAIL':
+      return { ...state, loadingCreateReview: false };
     default:
       return state;
   }
 };
 
 function ProductScreen() {
+  let reviewsRef = useRef();
+
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
   const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
-  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
-    //logger(reducer), {
-    loading: true,
-    product: [],
-    error: '',
-  });
+  const [{ loading, error, product, loadingCreateReview }, dispatch] =
+    useReducer(reducer, {
+      product: [],
+      loading: true,
+      error: '',
+    });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -57,7 +71,7 @@ function ProductScreen() {
   }, [slug]);
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { cart } = state;
+  const { cart, userInfo } = state;
   const addToCartHandler = async () => {
     const existItem = cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
@@ -75,6 +89,39 @@ function ProductScreen() {
 
     //Coming from react hook
     navigate('/cart');
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (!comment || !rating) {
+      toast.error('Please enter comment and rating');
+      return;
+    }
+    try {
+      const { data } = await axios.post(
+        `/api/products/${product._id}/reviews`,
+        { rating, comment, name: userInfo.firstname },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+
+      dispatch({
+        type: 'CREATE_SUCCESS',
+      });
+      toast.success('Avis posté');
+      product.reviews.unshift(data.review);
+      product.numReviews = data.numReviews;
+      product.rating = data.rating;
+      dispatch({ type: 'REFRESH_PRODUCT', payload: product });
+      window.scrollTo({
+        behavior: 'smooth',
+        top: reviewsRef.current.offsetTop,
+      });
+    } catch (error) {
+      toast.error(getError(error));
+      dispatch({ type: 'CREATE_FAIL' });
+    }
   };
 
   return loading ? (
@@ -151,6 +198,73 @@ function ProductScreen() {
           </Card>
         </Col>
       </Row>
+      <div className="my-3">
+        <h2 ref={reviewsRef}>Avis</h2>
+        <div className="mb-3">
+          {product.reviews.length === 0 && (
+            <MessageBox>Il n'y a pas d'avis pour ce produit</MessageBox>
+          )}
+        </div>
+        <ListGroup>
+          {product.reviews.map((review) => (
+            <ListGroup.Item key={review._id}>
+              <strong>{review.firstname}</strong>
+              <Rating rating={review.rating} caption=" "></Rating>
+              <p>{review.createdAt.substring(0, 10)}</p>
+              <p>{review.comment}</p>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+        <div className="my-3">
+          {userInfo ? (
+            <form onSubmit={submitHandler}>
+              <h2>Ecrire un avis</h2>
+              <Form.Group className="mb-3" controlId="rating">
+                <Form.Label>Note</Form.Label>
+                <Form.Select
+                  aria-label="Rating"
+                  value={rating}
+                  onChange={(e) => setRating(e.target.value)}
+                >
+                  <option value="">Selectionner...</option>
+                  <option value="1">1- Mauvais</option>
+                  <option value="2">2- Moyen</option>
+                  <option value="3">3- Bon</option>
+                  <option value="4">4- Très bon</option>
+                  <option value="5">5- Excellent</option>
+                </Form.Select>
+              </Form.Group>
+              <FloatingLabel
+                controlId="floatingTextarea"
+                label="Comments"
+                className="mb-3"
+              >
+                <Form.Control
+                  as="textarea"
+                  placeholder="Leave a comment here"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                />
+              </FloatingLabel>
+
+              <div className="mb-3">
+                <Button disabled={loadingCreateReview} type="submit">
+                  Poster
+                </Button>
+                {loadingCreateReview && <LoadingBox></LoadingBox>}
+              </div>
+            </form>
+          ) : (
+            <MessageBox>
+              {' '}
+              <Link to={`/signin?redirect=/product/${product.slug}`}>
+                Se connecter
+              </Link>{' '}
+              pour écrire un avis
+            </MessageBox>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
